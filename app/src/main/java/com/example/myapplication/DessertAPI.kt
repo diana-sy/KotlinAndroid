3a46c6a6526727a2b577097eb6e81c02996e4996
Diana <c0sydi00@students.oamk.fi>
2022-03-19 14:55:54 +0200

    mapping API to the app

--- app/src/main/java/com/example/myapplication/DessertAPI.kt
+++ app/src/main/java/com/example/myapplication/DessertAPI.kt
@@ -1,10 +1,12 @@
 package com.example.myapplication
 
+
+import retrofit2.Call
 import retrofit2.http.GET
 
 interface DessertAPI {
     @GET("/api/dessert/random_dessert")
-    fun getRandomDessert(){
+    fun getRandomDessert():Call<DessertData>
 
-    }
+
 }
